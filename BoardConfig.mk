c695f8e683d184a3102b66d92b11ebae3bf642a6
f100cleveland <dustin.rinne@gmail.com>
2016-02-29 21:07:28 -0600

    angler: TWRP brinup
    kang'd from TeamWin

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -133,3 +133,12 @@ EXTENDED_FONT_FOOTPRINT := true
 
 -include vendor/huawei/angler/BoardConfigVendor.mk
 
+# TWRP
+TW_THEME := portrait_hdpi
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+RECOVERY_SDCARD_ON_DATA := true
+BOARD_HAS_NO_REAL_SDCARD := true
+TW_INCLUDE_CRYPTO := true
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TARGET_RECOVERY_QCOM_RTC_FIX := true
